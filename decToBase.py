d0b410a1116a6d5bc1b7a52a20faf7ff66e95415
codeprogress <codeprogress1@gmail.com>
2013-06-17 15:55:09 -0400

    more changes

--- decToBase.py
+++ decToBase.py
@@ -4,10 +4,8 @@ def decToBase(n, b, iterCounter = 0):
     b: 2 <= int <= 16
     iterCounter: default 0.  Change to 1 if you do not want to test input.
     recursive implementation
-    testing this method using built in method int('num', base):
-        False not in [int(decToBase(i, x),x) == i \
-            for i in range(1,50000,37) for x in range(2,16)]
     '''
+    
     #check test conditions only on first iteration
     if iterCounter == 0:
         assert n >= 0 and 2 <= b <= 16 and type(n) in [int, long]
@@ -15,6 +13,9 @@ def decToBase(n, b, iterCounter = 0):
     #base case
     if n == 0:
         return ''
-    
+
+    #digits will be indexed into to get place digit
     digits = "0123456789ABCDEF"
+
+    #recursive call
     return decToBase(n/b, b, 1) + digits[n%b]
