4eb8962d33c4b7bfd7bab9cc77b2ed78f23af25c
CodeProgress <codeprogress1@gmail.com>
2014-07-26 23:46:19 -0400

    Update simpleSieve.py

--- simpleSieve.py
+++ simpleSieve.py
@@ -3,12 +3,13 @@ def simpleSieve(limit):
     limit: positive integer
     sacrifices speed for readability
     '''
-    sieve = [0 for x in xrange(limit + 1)]
+    limit += 1
+    knownPrimes = [0 for x in xrange(limit)]
     primes = []
-    for posPrime in xrange(2, limit + 1):
-        if sieve[posPrime] != 0:
-            continue
-        primes.append(posPrime)
-        for multiple in xrange(posPrime*2, limit + 1, posPrime):
-            sieve[multiple] = 1
+    for posPrime in xrange(2, limit):
+        if not knownPrimes[posPrime]:
+            primes.append(posPrime)
+            for primeMultiple in xrange(posPrime, limit, posPrime):
+                knownPrimes[primeMultiple] = 1
     return primes
+    
